5cedcdb8d89c54846583a67602ea7822eb48a16d
lawwang <2565662210@qq.com>
2020-08-11 16:57:59 +0800

    v1.0.0

--- tuling.py
+++ tuling.py
@@ -2,7 +2,7 @@ import  requests
 import json
 def tuling_reply(ask):
     ques = ask
-    #or your tuling_key that you should get from http://openapi.tuling123.com
+    #or your tuling_key that you should get from http://openapi.tuling123.com.
     tuling_key = "f222eaa9ede84cc6bfdd6f6d2ca4a589"
     tuling_url = "http://openapi.tuling123.com/openapi/api/v2"
     # or your id
