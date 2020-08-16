afb0c235162381934cb7f673764241929dc3ef3d
lawwang <2565662210@qq.com>
2020-08-16 12:04:07 +0800

    fix the doutula.com's bug

--- doutula.py
+++ doutula.py
@@ -6,17 +6,20 @@ import re
 #return ：fomat（like img gif..）
 #save as ：/tem/doutu.fomat
 def qiotu(keyword):
-    url = "http://www.doutula.com/search"
-    headers = {"keyword" : keyword}
+    url = "https://www.doutula.com/search"
+    parms = {"keyword" : keyword}
+    headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36 Edg/80.0.361.66"}
 
-    r = requests.get(url,headers)
+    r = requests.get(url,params=parms,headers = headers)
+    #print(r.text)
     if r.status_code != 200:
         print("web request fail：%d" % r.status_code)
         return False
     else:
         soup = bs4.BeautifulSoup(r.text,"html.parser")
-        reg = re.compile("http://img\.doutula\.com/production/uploads/image/.*")
+        reg = re.compile("http://img\.doutula\.com/production/uploads/image/.+\.(png|gif|jpg)")
         imgtaglist = soup.find_all("img",{"data-backup" : reg})
+        #print(imgtaglist)
         if not len(imgtaglist):
             print("find emoji fail")
             return False
@@ -25,7 +28,7 @@ def qiotu(keyword):
             imgurl = imgtag["data-backup"]
             fomat = re.search("\w\w\w$",imgurl).group()
             try:
-                f = open("/tmp/doutu.%s" % fomat,"wb")
+                f = open("./doutu.%s" % (fomat),"wb")
                 data = requests.get(imgurl)
                 print("find emoji: %s" % imgurl)
                 f.write(data.content)
--- wx_bot.py
+++ wx_bot.py
@@ -2,14 +2,14 @@ from wxpy import *
 import tuling
 import doutula
 
-EN_tuling = False#when this is True,you'll auto replying. 
+EN_tuling = True#when this is True,you'll auto replying. 
 EN_dotula = True#when this is True,you'll get emoji fighting feature.
-IMG_NUM = 2#the number of emoji.
+IMG_NUM = 1 #the number of emoji.
 
 bot = Bot(cache_path=True)
-friend0 = bot.search("your friend's nick name")[0]#turn the string to your real wechat friend's nick name.and you could add other friends like friend1 or friend2 etc.but you should add 'friend1' or 'friend2' to the array 'accesslist' below.
-group0 = bot.search("group")[0] #just like friend0.
-accesslist = [friend0,group0] #where you add your friend or group.
+friend0 = bot.search("天使")[0]#turn the string to your real wechat friend's nick name.and you could add other friends like friend1 or friend2 etc.but you should add 'friend1' or 'friend2' to the array 'accesslist' below.
+#group0 = bot.search("group")[0] #just like friend0.
+accesslist = [friend0] #where you add your friend or group.
 @bot.register(msg_types="Text")
 def auto_rep(msg):
     sender = msg.sender
@@ -20,7 +20,7 @@ def auto_rep(msg):
             bot.file_helper.send_msg("%s: " % sender.name + msg.text)
         if EN_tuling:
             rep_text = tuling.tuling_reply(msg.text)
-            sender.send_msg("【わはは】：%s " % rep_text)
+            sender.send_msg("【robot】：%s " % rep_text)
         if EN_dotula:
             i = IMG_NUM
             while i:
@@ -29,7 +29,7 @@ def auto_rep(msg):
                     sender.send_msg("there is no emoji!")
                 else:
                     rep_img_format = re
-                    sender.send_image("/tmp/doutu.%s" % rep_img_format)
+                    sender.send_image("./doutu.%s" % (rep_img_format))
                     sender.send_file
                     print("send is ok")
                 i = i-1
